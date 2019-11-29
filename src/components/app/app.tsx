65166e7cfcbf56e70500694977a5f41848f863e8
Dalton Woods <dcwds@protonmail.com>
2019-11-29 14:45:13 -0500

    Clean up index and app component. App test now passes.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,16 +1,40 @@
-import React, { FC } from "react"
+import React, { FC, Fragment } from "react"
 import { Box } from "rebass"
+
+import { Provider } from "react-redux"
+import store from "../../store"
+
+import { ThemeProvider } from "emotion-theming"
+import { Global, css } from "@emotion/core"
+import theme from "../../styles/theme"
+
 import Header from "../header"
 import Posts from "../posts"
 
 const App: FC = () => {
   return (
-    <Box variant="styles.root">
-      <Header />
-      <Box as="main" sx={{ p: 4 }}>
-        <Posts />
-      </Box>
-    </Box>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Fragment>
+          <Global
+            styles={css`
+              html,
+              body {
+                height: 100vh;
+                margin: 0;
+                min-height: 100vh;
+              }
+            `}
+          />
+          <Box variant="styles.root">
+            <Header />
+            <Box as="main" sx={{ p: 4 }}>
+              <Posts />
+            </Box>
+          </Box>
+        </Fragment>
+      </ThemeProvider>
+    </Provider>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,10 @@
-import React, { Fragment } from "react"
+import React from "react"
 import ReactDOM from "react-dom"
 import App from "./components/app"
 
-import { Provider } from "react-redux"
-import store from "./store"
-
-import { ThemeProvider } from "emotion-theming"
-import { Global, css } from "@emotion/core"
-import theme from "./styles/theme"
-
 import * as serviceWorker from "./serviceWorker"
 
-ReactDOM.render(
-  <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <Fragment>
-        <Global
-          styles={css`
-            html,
-            body {
-              height: 100vh;
-              margin: 0;
-              min-height: 100vh;
-            }
-          `}
-        />
-        <App />
-      </Fragment>
-    </ThemeProvider>
-  </Provider>,
-  document.getElementById("root")
-)
+ReactDOM.render(<App />, document.getElementById("root"))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
