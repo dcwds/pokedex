5d180e800fe30b0d22513489ac56b1385061f607
Dalton Woods <dcwds@protonmail.com>
2020-01-21 02:52:48 -0500

    Add type icon creator attribution and mobile styles in Footer component.

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,12 +4,30 @@ import { Box, Text } from "rebass"
 
 const Footer: FC = () => (
   <Box as="footer" sx={{ bg: "muted" }}>
-    <Box variant="styles.container" sx={{ px: [2, 5], py: [2, 4] }}>
-      <Text as="p">
+    <Box
+      variant="styles.container"
+      sx={{
+        alignItems: "center",
+        display: ["block", null, "flex"],
+        justifyContent: "space-between",
+        textAlign: ["center", null, null],
+        px: [2, 5],
+        py: [2, 4]
+      }}
+    >
+      <Text as="p" sx={{ mb: [1, null, null] }}>
         Made by{" "}
         <ExternalLink href="https://github.com/dcwds">dcwds</ExternalLink>
       </Text>
-      <Text as="p">Pokémon are trademarks of Nintendo.</Text>
+      <Box sx={{ textAlign: [null, null, "right"] }}>
+        <Text as="p">Pokémon are trademarks of Nintendo.</Text>
+        <Text as="p">
+          Pokémon type icons by{" "}
+          <ExternalLink href="https://thenounproject.com/ilariabernareggi">
+            Ilaria Bernareggi
+          </ExternalLink>
+        </Text>
+      </Box>
     </Box>
   </Box>
 )
